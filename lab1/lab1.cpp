c34ee61d872c7c57289130578282702fd83838a3
qq17 <54769903+qq17@users.noreply.github.com>
2021-02-17 03:40:47 +0300

    lab1 with bash, need to add checks

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,59 +1,60 @@
-#include <iostream>
+#include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 
 using namespace std;
 
-//int flag1 = 0;
-//int flag2 = 0;
-
-void * f1(void *fl1)
+struct targs
 {
-    while(*(int *)fl1 == 0)
+    int flag;
+};
+
+void * f1(void *arg)
+{
+    targs *args = (targs *) arg;
+
+    while(args->flag == 0)
     {
-        cout << "1" << endl;
-        sleep(1);
+        putchar('1'); //check
+        fflush(stdout); //check
+        sleep(1); //check
     }
-    //int r [1] = {55};
-    pthread_exit((void *)55);
-    //return "st";
+    pthread_exit((void *)55); //check
 }
 
-void * f2(void *fl2)
+void * f2(void *arg)
 {
-    while(*(int *)fl2 == 0)
+    targs *args = (targs *) arg;
+    
+    while(args->flag == 0)
     {
-        cout << "2" << endl;
-        sleep(1);
+        putchar('2'); //check
+        fflush(stdout); //check
+        sleep(1); //check
     }
-    //int r [1] = {77};
-    pthread_exit((void *)77);
-    //return "ri";
+    pthread_exit((void *)77); //check
 }
 
 int main()
 {
-    int fl1 = 0;
-    int fl2 = 0;
-    //int *r1;
-    //int *r2;
+    targs arg1;
+    targs arg2;
+    arg1.flag = 0;
+    arg2.flag = 0;
     int r1;
     int r2;
     pthread_t t1;
     pthread_t t2;
-    pthread_create(&t1, NULL, f1, &fl1);
-    pthread_create(&t2, NULL, f2, &fl2);
-    getchar();
+    pthread_create(&t1, NULL, f1, &arg1); // check
+    pthread_create(&t2, NULL, f2, &arg2); // check
+    getchar(); //check
 
-    fl1 = 1;
-    fl2 = 1;
-    pthread_join(t1, (void **)&r1);
-    pthread_join(t2, (void **)&r2);
-    cout << r1 << endl;
-    cout << r2 << endl;
-    //getchar();
-
+    arg1.flag = 1;
+    arg2.flag = 1;
+    pthread_join(t1, (void **)&r1); //check
+    pthread_join(t2, (void **)&r2); //check
+    printf("exitcode from t1 = %d\n", r1); //check
+    printf("exitcode from t2 = %d\n", r2); //check
     
-    getchar();
     return 0;
 }
