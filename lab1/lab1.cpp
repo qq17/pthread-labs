e3f842d5c32a296d314a1cefb4b0363413687b3b
qq17 <54769903+qq17@users.noreply.github.com>
2021-02-17 17:59:26 +0300

    added checks in lab1

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
 
 using namespace std;
 
@@ -12,12 +13,25 @@ struct targs
 void * f1(void *arg)
 {
     targs *args = (targs *) arg;
+    int r;
 
     while(args->flag == 0)
     {
-        putchar('1'); //check
-        fflush(stdout); //check
-        sleep(1); //check
+        r = putchar('1'); //check
+        if (r == EOF)
+        {
+            perror("putchar error: ");
+        }
+        r = fflush(stdout); //check
+        if (r == EOF)
+        {
+            perror("fflush error: ");
+        }
+        r = sleep(1); //check
+        if (r != 0)
+        {
+            perror("sleep error: ");
+        }
     }
     pthread_exit((void *)55); //check
 }
@@ -25,12 +39,25 @@ void * f1(void *arg)
 void * f2(void *arg)
 {
     targs *args = (targs *) arg;
-    
+    int r;
+
     while(args->flag == 0)
     {
-        putchar('2'); //check
-        fflush(stdout); //check
-        sleep(1); //check
+        r = putchar('2'); //check
+        if (r == EOF)
+        {
+            perror("putchar error: ");
+        }
+        r = fflush(stdout); //check
+        if (r == EOF)
+        {
+            perror("fflush error: ");
+        }
+        r = sleep(1); //check
+        if (r != 0)
+        {
+            perror("sleep error: ");
+        }
     }
     pthread_exit((void *)77); //check
 }
@@ -43,18 +70,43 @@ int main()
     arg2.flag = 0;
     int r1;
     int r2;
+    int r;
     pthread_t t1;
     pthread_t t2;
-    pthread_create(&t1, NULL, f1, &arg1); // check
-    pthread_create(&t2, NULL, f2, &arg2); // check
+    r = pthread_create(&t1, NULL, f1, &arg1); // check
+    if (r != 0)
+    {
+        printf("pthread_create error: %s", strerror(r));
+    }
+    r = pthread_create(&t2, NULL, f2, &arg2); // check
+    if (r != 0)
+    {
+        printf("pthread_create error: %s", strerror(r));
+    }
     getchar(); //check
 
     arg1.flag = 1;
     arg2.flag = 1;
-    pthread_join(t1, (void **)&r1); //check
-    pthread_join(t2, (void **)&r2); //check
-    printf("exitcode from t1 = %d\n", r1); //check
-    printf("exitcode from t2 = %d\n", r2); //check
+    r = pthread_join(t1, (void **)&r1); //check
+    if (r != 0)
+    {
+        printf("pthread_join error: %s", strerror(r));
+    }
+    r = pthread_join(t2, (void **)&r2); //check
+    if (r != 0)
+    {
+        printf("pthread_join error: %s", strerror(r));
+    }
+    r = printf("exitcode from t1 = %d\n", r1); //check
+    if (r < 0)
+    {
+        perror("printf error: ");
+    }
+    r = printf("exitcode from t2 = %d\n", r2); //check
+    if (r < 0)
+    {
+        perror("printf error: ");
+    }
     
     return 0;
 }
