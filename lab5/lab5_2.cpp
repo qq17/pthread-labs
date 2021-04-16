7ec0a03749b658d07efa92c919ae681db54f0aeb
qq17 <54769903+qq17@users.noreply.github.com>
2021-04-16 19:59:49 +0300

    lab5 send

--- lab5/lab5_2.cpp
+++ lab5/lab5_2.cpp
@@ -22,15 +22,32 @@ void * thread_func(void *arg)
 
     while(args->flag == 0)
     {
-        sem_wait(sem);
-    	fprintf(fd, "%c", c);
-        putchar(c);
-        fflush(stdout);
-        fflush(fd);
-        sem_post(sem);
+        r = sem_wait(sem);
+        if (r == -1)
+        {
+            perror("lab5_2 sem_wait error");
+            pthread_exit((void *)8);
+        }
+
+        for (int i=0; i<3; ++i)
+        {
+            fprintf(fd, "%c", c);
+            putchar(c);
+            fflush(stdout);
+            fflush(fd);
+            sleep(1);
+        }
+
+        r = sem_post(sem);
+        if (r == -1)
+        {
+            perror("lab5_2 sem_post error");
+            pthread_exit((void *)9);
+        }
+
         sleep(1);
     }
-    
+    pthread_exit((void *)0);
 }
 
 int main()
@@ -38,14 +55,21 @@ int main()
     targs arg;
     arg.flag = 0;
     int r;
+    int exitcode;
     pthread_t t;
 
     sem = sem_open("/semaphore", O_CREAT, 0644, 1);
+    if (sem == SEM_FAILED)
+    {
+        perror("lab5_2 sem_open error");
+        return 1;
+    }
 
     fd = fopen("lab5_file.txt", "a+");
     if (fd == NULL)
     {
-    	perror("lab5_2 fopen error");
+        perror("lab5_2 fopen error");
+        return 2;
     }
 
     r = pthread_create(&t, NULL, thread_func, &arg);
@@ -53,28 +77,43 @@ int main()
     {
         printf("lab5_2 pthread_create error: %s", strerror(r));
         printf("\n");
+        return 3;
     }
 
     getchar();
 
     arg.flag = 1;
 
-    r = pthread_join(t, NULL);
+    r = pthread_join(t, (void **)&exitcode);
     if (r != 0)
     {
         printf("lab5_2 pthread_join error: %s", strerror(r));
         printf("\n");
+        return 4;
     }
+
+    printf("lab5_2 exitcode from thread_func: %d", exitcode);
 
     r = fclose(fd);
     if (r == EOF)
     {
-    	perror("lab5_2 fclose error");
+        perror("lab5_2 fclose error");
+        return 5;
     }
 
-    sem_close(sem);
+    r = sem_close(sem);
+    if (r == -1)
+    {
+        perror("lab5_2 sem_close error");
+        return 6;
+    }
 
-    sem_unlink("/semaphore");
+    r = sem_unlink("/semaphore");
+    if (r == -1)
+    {
+        perror("lab5_2 sem_unlink error");
+        return 7;
+    }
     
     return 0;
 }
