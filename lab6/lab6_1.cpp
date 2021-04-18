ed175bc8b96e2464ac94dfffc1195514f4057d8c
qq17 <54769903+qq17@users.noreply.github.com>
2021-04-18 23:30:34 +0300

    lab6 send

--- lab6/lab6_1.cpp
+++ lab6/lab6_1.cpp
@@ -6,6 +6,7 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctime>
 
 #define SHM_SIZE 80
 
@@ -22,20 +23,34 @@ int *addr;
 void * thread_func(void *arg)
 {
     targs *args = (targs *) arg;
-    char c[] = "message";
     int r;
+
+    char buffer[SHM_SIZE];
+    time_t rawtime;
+    tm *loctime;
 
     while(args->flag == 0)
     {
-        for (int i=0; i<3; ++i)
-        {
-            memcpy(addr, c, strlen(c));
-            sleep(1);
-        }
+        rawtime = time(0);
+        loctime = localtime(&rawtime);
+        r = strftime(buffer, SHM_SIZE, "%Y.%m.%d %H:%M:%S", loctime);
+
+        memcpy(addr, buffer, SHM_SIZE);
+        printf("%s\n", buffer);
 
         r = sem_post(sem_write);
+        if (r == -1)
+        {
+            perror("lab6_1 sem_post error");
+            pthread_exit((void *)15);
+        }
 
         r = sem_wait(sem_read);
+        if (r == -1)
+        {
+            perror("lab6_1 sem_wait error");
+            pthread_exit((void *)16);
+        }
 
         sleep(1);
     }
@@ -51,18 +66,46 @@ int main()
     pthread_t t;
 
     shm = shm_open("/lab6_shared_memory", O_CREAT | O_RDWR, 0644);
+    if (r == -1)
+    {
+        perror("lab6_1 shm_open error");
+        return 1;
+    }
+
     ftruncate(shm, SHM_SIZE);
+    if (r == -1)
+    {
+        perror("lab6_1 ftruncate error");
+        return 2;
+    }
+
     addr = (int *)mmap(NULL, SHM_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shm, 0);
+    if (addr == MAP_FAILED)
+    {
+        perror("lab6_1 mmap error");
+        return 3;
+    }
 
     sem_write = sem_open("/lab6_semaphore_write", O_CREAT, 0644, 0);
+    if (sem_write == SEM_FAILED)
+    {
+        perror("lab6_1 sem_open error");
+        return 4;
+    }
+
     sem_read = sem_open("/lab6_semaphore_read", O_CREAT, 0644, 0);
+    if (sem_read == SEM_FAILED)
+    {
+        perror("lab6_1 sem_open error");
+        return 5;
+    }
 
     r = pthread_create(&t, NULL, thread_func, &arg);
     if (r != 0)
     {
         printf("lab6_1 pthread_create error: %s", strerror(r));
         printf("\n");
-        return 3;
+        return 6;
     }
 
     getchar();
@@ -74,24 +117,59 @@ int main()
     {
         printf("lab6_1 pthread_join error: %s", strerror(r));
         printf("\n");
-        return 4;
+        return 7;
     }
 
     printf("lab6_1 exitcode from thread_func: %d\n", exitcode);
 
-
     r = sem_close(sem_write);
+    if (r == -1)
+    {
+        perror("lab6_1 sem_close error");
+        return 8;
+    }
 
     r = sem_unlink("/lab6_semaphore_write");
+    if (r == -1)
+    {
+        perror("lab6_1 sem_unlink error");
+        return 9;
+    }
 
     r = sem_close(sem_read);
+    if (r == -1)
+    {
+        perror("lab6_1 sem_close error");
+        return 10;
+    }
 
     r = sem_unlink("/lab6_semaphore_read");
+    if (r == -1)
+    {
+        perror("lab6_1 sem_unlink error");
+        return 11;
+    }
 
-    munmap(addr, sizeof(char));
+    r = munmap(addr, sizeof(char));
+    if (r == -1)
+    {
+        perror("lab6_1 munmap error");
+        return 12;
+    }
+
     close(shm);
+    if (r == -1)
+    {
+        perror("lab6_1 close error");
+        return 13;
+    }
 
     shm_unlink("/lab6_shared_memory");
+    if (r == -1)
+    {
+        perror("lab6_1 shm_unlink error");
+        return 14;
+    }
     
     return 0;
 }
--- lab6/lab6_2.cpp
+++ lab6/lab6_2.cpp
@@ -22,22 +22,27 @@ int *addr;
 void * thread_func(void *arg)
 {
     targs *args = (targs *) arg;
-    char c[SHM_SIZE];
+    char buffer[SHM_SIZE];
     int r;
-
-    r = sem_wait(sem_write);
 
     while(args->flag == 0)
     {
-        for (int i=0; i<3; ++i)
+        r = sem_wait(sem_write);
+        if (r == -1)
         {
-            memcpy(c, addr, SHM_SIZE);
-            printf("%s\n", c);
-            fflush(stdout);
-            sleep(1);
+            perror("lab6_2 sem_wait error");
+            pthread_exit((void *)15);
         }
 
+        memcpy(buffer, addr, SHM_SIZE);
+        printf("%s\n", buffer);
+
         r = sem_post(sem_read);
+        if (r == -1)
+        {
+            perror("lab6_2 sem_post error");
+            pthread_exit((void *)16);
+        }
     }
     pthread_exit((void *)0);
 }
@@ -51,18 +56,46 @@ int main()
     pthread_t t;
 
     shm = shm_open("/lab6_shared_memory", O_CREAT | O_RDWR, 0644);
+    if (r == -1)
+    {
+        perror("lab6_2 shm_open error");
+        return 1;
+    }
+
     ftruncate(shm, SHM_SIZE);
+    if (r == -1)
+    {
+        perror("lab6_2 ftruncate error");
+        return 2;
+    }
+
     addr = (int *)mmap(NULL, SHM_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, shm, 0);
+    if (addr == MAP_FAILED)
+    {
+        perror("lab6_2 mmap error");
+        return 3;
+    }
 
     sem_write = sem_open("/lab6_semaphore_write", O_CREAT, 0644, 0);
+    if (sem_write == SEM_FAILED)
+    {
+        perror("lab6_2 sem_open error");
+        return 4;
+    }
+
     sem_read = sem_open("/lab6_semaphore_read", O_CREAT, 0644, 0);
+    if (sem_read == SEM_FAILED)
+    {
+        perror("lab6_2 sem_open error");
+        return 5;
+    }
 
     r = pthread_create(&t, NULL, thread_func, &arg);
     if (r != 0)
     {
         printf("lab6_2 pthread_create error: %s", strerror(r));
         printf("\n");
-        return 3;
+        return 6;
     }
 
     getchar();
@@ -74,24 +107,59 @@ int main()
     {
         printf("lab6_2 pthread_join error: %s", strerror(r));
         printf("\n");
-        return 4;
+        return 7;
     }
 
     printf("lab6_2 exitcode from thread_func: %d\n", exitcode);
 
-
     r = sem_close(sem_write);
+    if (r == -1)
+    {
+        perror("lab6_2 sem_close error");
+        return 8;
+    }
 
     r = sem_unlink("/lab6_semaphore_write");
+    if (r == -1)
+    {
+        perror("lab6_2 sem_unlink error");
+        return 9;
+    }
 
     r = sem_close(sem_read);
+    if (r == -1)
+    {
+        perror("lab6_2 sem_close error");
+        return 10;
+    }
 
     r = sem_unlink("/lab6_semaphore_read");
+    if (r == -1)
+    {
+        perror("lab6_2 sem_unlink error");
+        return 11;
+    }
 
-    munmap(addr, sizeof(char));
+    r = munmap(addr, sizeof(char));
+    if (r == -1)
+    {
+        perror("lab6_2 munmap error");
+        return 12;
+    }
+
     close(shm);
+    if (r == -1)
+    {
+        perror("lab6_2 close error");
+        return 13;
+    }
 
     shm_unlink("/lab6_shared_memory");
+    if (r == -1)
+    {
+        perror("lab6_2 shm_unlink error");
+        return 14;
+    }
     
     return 0;
 }
